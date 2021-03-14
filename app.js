f8b87ff87b4747893534946b97a149f544f44ab2
Chenyu Wang <chouneyou@gmail.com>
2021-03-14 12:29:01 -0400

    add girignore

--- app.js
+++ app.js
@@ -117,6 +117,12 @@ List.findOneAndUpdate({name:listName},{$pull:{items:{_id:checkedItemId}}},functi
 }
 });
 
-app.listen(3000, function(){
-  console.log("Server started on port 3000.");
+let port=process.env.PORT;
+if(port==null || port==""){
+    port=3000;
+}
+
+
+app.listen(port, function(){
+  console.log("Server started.");
 });
