4ab063018da6ce72418a3cb1739108aa056a4fe7
Chenyu Wang <chouneyou@gmail.com>
2021-03-14 12:47:15 -0400

    modify

--- app.js
+++ app.js
@@ -120,8 +120,7 @@ List.findOneAndUpdate({name:listName},{$pull:{items:{_id:checkedItemId}}},functi
 let port=process.env.PORT;
 if(port==null || port==""){
     port=3000;
-}
-
+};
 
 app.listen(port, function(){
   console.log("Server started.");
