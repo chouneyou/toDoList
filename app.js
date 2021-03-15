5f6715f4cb5ee7f72b36a3203f58c6a711345c39
Chenyu Wang <chouneyou@gmail.com>
2021-03-15 13:50:34 -0400

    change Procfile & app.listen

--- app.js
+++ app.js
@@ -117,11 +117,6 @@ List.findOneAndUpdate({name:listName},{$pull:{items:{_id:checkedItemId}}},functi
 }
 });
 
-let port=process.env.PORT;
-if(port==null || port==""){
-    port=3000;
-};
-
-app.listen(port, function(){
-  console.log("Server started.");
-});
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
